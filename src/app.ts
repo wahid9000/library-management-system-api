f829d47027aed0f0285899f04a9621f8cd353c4b
wahid_9000 <wahidchowdhury9000@gmail.com>
2025-06-21 21:47:23 +0600

    done: basic server implmented with mongoose and mongodb setup

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ export const app: Application = express();
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  console.log("Welcome to Library Management System");
+  res.send("Welcome to Library Management System");
 });
 
 export default app;
