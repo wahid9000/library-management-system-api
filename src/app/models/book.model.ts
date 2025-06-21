d41711a5c68784a259d40e8d0570bdd6db11c6ac
wahid_9000 <wahidchowdhury9000@gmail.com>
2025-06-21 22:06:10 +0600

    added: book model

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,25 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/book.interface";
+
+const bookSchema = new Schema<IBook>({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  genre: {
+    type: String,
+    enum: [
+      "FICTION",
+      "NON_FICTION",
+      "SCIENCE",
+      "HISTORY",
+      "BIOGRAPHY",
+      "FANTASY",
+    ],
+    required: true,
+  },
+  isbn: { type: String, required: true, unique: true },
+  description: { type: String },
+  copies: { type: Number, required: true },
+  available: { type: Boolean, default: true },
+});
+
+export const Book = model<IBook>("Book", bookSchema);
