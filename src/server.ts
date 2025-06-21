01a01ee86f6602c0f5071acffb764c8205091628
wahid_9000 <wahidchowdhury9000@gmail.com>
2025-06-21 21:49:21 +0600

    done: basic server implmented with mongoose and mongodb setup

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import mongoose from "mongoose";
 import "dotenv/config";
 import app from "./app";
+import { Server } from "http";
 const port = 5000;
+let server: Server;
 
 async function main() {
-  await mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.wy87kp4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-  );
-  console.log('Connected to MongoDB successfully !!!');
-  const server = app.listen(port, () => {
-    console.log(`Connected to port ${port}`);
-  });
+  try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.wy87kp4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+    console.log("Connected to MongoDB successfully !!!");
+    server = app.listen(port, () => {
+      console.log(`Connected to port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 main();
