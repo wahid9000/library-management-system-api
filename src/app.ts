15d68608890f856dfffdb373acd5311f7cf471a0
wahid_9000 <wahidchowdhury9000@gmail.com>
2025-07-03 23:19:30 +0600

    added: cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from "express";
 import { bookRouter } from "./app/controllers/book.controller";
 import { borrowRouter } from "./app/controllers/borrow.controller";
+import cors from "cors";
 
 export const app: Application = express();
 
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api/books", bookRouter);
 app.use("/api/borrow", borrowRouter);
